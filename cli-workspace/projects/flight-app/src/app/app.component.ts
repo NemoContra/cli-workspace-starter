60f7e4c8bfe2d68ca11c250d4fd491920439ea5e
Michael Hladky <michael@hladky.at>
2018-06-12 02:37:03 +0200

    implement increment action and dispatch it

--- cli-workspace/projects/flight-app/src/app/app.component.ts
+++ cli-workspace/projects/flight-app/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
 import { Store } from '@ngrx/store';
 import { State } from './+state/index';
+import { CounterIncrementAction } from './+state/app.actions';
+import * as fromApp from './+state/app.selectors';
 
 @Component({
   selector: 'fl-app-root',
@@ -12,6 +14,12 @@ export class AppComponent {
   count$: Observable<number>;
 
   constructor(private store: Store<State>) {
-    this.count$ = this.store.select(state => state.app.count);
+    this.count$ = this.store.select(fromApp.getCount);
+  }
+
+  countUp() {
+    const action = new CounterIncrementAction({incrementBy:1});
+    console.log(action)
+    this.store.dispatch(action);
   }
 }
