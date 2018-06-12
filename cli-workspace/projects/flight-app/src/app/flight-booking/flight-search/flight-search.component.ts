d016f6659634443b26e40b68b9d49a5f35f63a55
Michael Hladky <michael@hladky.at>
2018-06-12 04:02:39 +0200

    implement increment flights feature store and add flights state

--- cli-workspace/projects/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
+++ cli-workspace/projects/flight-app/src/app/flight-booking/flight-search/flight-search.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import {Flight, FlightService} from '@flight-workspace/flight-api';
+import {Store} from '@ngrx/store';
+import * as fromFlightBooking from '../+state/flight-booking.selector';
+import {
+  FlightBookingActions, FlightBookingActionTypes,
+  FlightsLoaded
+} from '../+state/flight-booking.actions';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'fl-app-flight-search',
@@ -21,8 +28,12 @@ export class FlightSearchComponent implements OnInit {
     5: true
   };
 
+  flights$: Observable<Flight[]>;
+
   constructor(
-    private flightService: FlightService) {
+    private flightService: FlightService,
+  private store: Store<any>) {
+    this.flights$ = this.store.select(fromFlightBooking.getFlights)
   }
 
   ngOnInit() {
@@ -33,7 +44,16 @@ export class FlightSearchComponent implements OnInit {
       return;
     }
 
-    this.flightService.load(this.from, this.to, this.urgent);
+    this.flightService.find(this.from, this.to, this.urgent)
+      .subscribe(
+        flights => {
+          const action: FlightsLoaded = {
+            type: FlightBookingActionTypes.FlightsLoaded,
+            payload: {flights: flights}
+          };
+          this.store.dispatch(action);
+        }
+      );
   }
 
   delay(): void {
