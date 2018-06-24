90f8424cd7c46ee5d6ba9d94a928e82593f18c2b
Michael Hladky <michael@hladky.at>
2018-06-24 17:24:34 +0200

    implement effect for save as optimistic update event

--- cli-workspace/projects/flight-app/src/app/flight-booking/flight-edit/flight-edit.component.ts
+++ cli-workspace/projects/flight-app/src/app/flight-booking/flight-edit/flight-edit.component.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import {Flight, FlightService} from '@flight-workspace/flight-api';
+import {Store} from '@ngrx/store';
+import {pluck, switchMap} from 'rxjs/operators';
+import {State as FlightBookingState} from '../+state/flight-booking.reducer';
+import {
+  FlightBookingActionTypes,
+  UpdateFlight
+} from '../+state/flight-booking.actions';
 
 @Component({
   selector: 'fl-app-flight-edit',
@@ -10,7 +19,30 @@ export class FlightEditComponent implements OnInit {
   showDetails: string;
   showWarning = false;
 
-  constructor(private route: ActivatedRoute) {
+  editForm: FormGroup;
+
+  constructor(private store: Store<any>, private route: ActivatedRoute, private fb: FormBuilder, private flightService: FlightService) {
+    this.editForm = this.fb.group({
+      'id': [],
+      'from': [],
+      'to': [],
+      'date': []
+    });
+
+    this.route.params
+      .pipe(
+        pluck('id'),
+        switchMap((id: string) => this.flightService.findById(id))
+      )
+      .subscribe(flight => this.editForm.patchValue(flight));
+  }
+
+  save(flight: Flight) {
+    const action: UpdateFlight = {
+      type: FlightBookingActionTypes.UpdateFlight,
+      payload: {flight: flight}
+    }
+    this.store.dispatch(action);
   }
 
   ngOnInit() {
