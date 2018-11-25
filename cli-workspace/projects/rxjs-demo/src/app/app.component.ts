4e721a579fe665e412da4a0890da635ed810df3c
Florian Tischler <florian.tischler@gmx.net>
2018-11-25 14:54:43 +0100

    Start for rxjs live demo

--- cli-workspace/projects/rxjs-demo/src/app/app.component.ts
+++ cli-workspace/projects/rxjs-demo/src/app/app.component.ts
@@ -9,27 +9,5 @@ import { concatMap, delay, exhaustMap, switchMap, tap } from 'rxjs/operators';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  public updateCounter = 0;
-  public data$: Observable<Flight[]>;
-
-  private flights$: Observable<Flight[]> = this.httpClient.get<Flight[]>('/api/flight');
-  private clickSubject = new Subject<boolean>();
-
-  constructor(private httpClient: HttpClient) { }
-
-  ngOnInit(): void {
-    this.data$ = this.clickSubject.pipe(
-      switchMap(() => this.flights$),
-      tap(() => this.updateCounter++)
-    );
-  }
-
-  loadFlights(): void {
-    this.clickSubject.next(true);
-  }
-
-  resetCounter(): void {
-    this.updateCounter = 0;
-  }
+export class AppComponent {
 }
