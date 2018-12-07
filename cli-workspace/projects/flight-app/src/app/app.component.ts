abfa8b6ff0498eeb569538a7f5a7b20cc9207d8b
Marco Hämmerle <m.haemmerle@nemocontra.de>
2018-12-07 11:54:48 +0100

    Second exercise: Using the Update Service

--- cli-workspace/projects/flight-app/src/app/app.component.ts
+++ cli-workspace/projects/flight-app/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'fl-app-root',
@@ -6,4 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private snackBar: MatSnackBar, private swUpdate: SwUpdate) {
+    this.setupUpdates();
+  }
+
+  setupUpdates() {
+    this.swUpdate.available.subscribe(u => {
+      this.swUpdate.activateUpdate().then(e => {
+        this.snackBar.open("App updated -- please reload!", "OK");
+      });
+    });
+
+    this.swUpdate.checkForUpdate();
+
+  }
 }
