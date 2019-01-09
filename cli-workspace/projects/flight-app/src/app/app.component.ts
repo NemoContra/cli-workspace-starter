7a273d4f164a1bf23c0b1bf425becb5359d00282
Florian Tischler <florian.tischler@gmx.net>
2019-01-09 23:52:22 +0100

    Created Starter

--- cli-workspace/projects/flight-app/src/app/app.component.ts
+++ cli-workspace/projects/flight-app/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'fl-app-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'Welcome to flight-app!';
 }
--- cli-workspace/projects/flight-app/src/app/flight-booking/flight-booking.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'fl-app-flight-booking',
-  templateUrl: './flight-booking.component.html'
-})
-export class FlightBookingComponent {
-}
